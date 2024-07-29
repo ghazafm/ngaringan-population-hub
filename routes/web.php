5f9e43bfff64ff19ab2ea7ce05af898b11a2942f
ghazafm <fauzan.ghaza@gmail.com>
2024-07-29 14:47:55 +0700

    add security

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,26 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Filament\Facades\Filament;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('/dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/dashboard',[DashboardController::class, 'index'])->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
