9047e137d302cf53b1125ca89f3e613aefe5c8f7
MeishaPD <putradewanm@student.ub.ac.id>
2024-07-31 15:05:53 +0700

    modified:   app/Filament/Dasawisma/Resources/KartuKeluargaResource.php 	modified:   app/Filament/Dasawisma/Resources/KehamilanResource.php 	modified:   app/Filament/Dasawisma/Resources/KelahiranResource.php 	modified:   app/Filament/Dasawisma/Resources/KematianResource.php 	modified:   app/Filament/Dasawisma/Resources/PendudukResource.php 	modified:   app/Filament/Dasawisma/Resources/RumahResource.php

--- app/Filament/Dasawisma/Resources/KehamilanResource.php
+++ app/Filament/Dasawisma/Resources/KehamilanResource.php
@@ -65,7 +65,7 @@ class KehamilanResource extends Resource
                 ->sortable()
                 ->searchable(),
                 Tables\Columns\TextColumn::make('nama_suami')
-                    ->label('Nama Suami')
+                    ->label('Nama Ayah')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('ibu.nama')
                     ->label('Nama Ibu')
@@ -80,7 +80,7 @@ class KehamilanResource extends Resource
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
-            ->defaultSort('created_at', 'desc') // Mengurutkan berdasarkan kolom created_at secara menurun
+            ->defaultSort('updated_at', 'desc') // Mengurutkan berdasarkan kolom created_at secara menurun
             ->filters([
                 //
             ])
