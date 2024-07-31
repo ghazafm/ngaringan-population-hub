3d3d31c9bf36ec9325ebf7d98341df5014838f2b
ghazafm <fauzan.ghaza@gmail.com>
2024-07-31 18:31:43 +0700

    fix last database

--- app/Filament/Dasawisma/Resources/PendudukResource/Pages/EditPenduduk.php
+++ app/Filament/Dasawisma/Resources/PendudukResource/Pages/EditPenduduk.php
@@ -5,6 +5,8 @@ namespace App\Filament\Dasawisma\Resources\PendudukResource\Pages;
 use App\Filament\Dasawisma\Resources\PendudukResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use App\Models\Penduduk;
+use App\Models\Rumah;
 
 class EditPenduduk extends EditRecord
 {
@@ -14,7 +16,16 @@ class EditPenduduk extends EditRecord
     {
         return [
             Actions\ViewAction::make(),
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->action(function (Penduduk $record) {
+                // Update related Penduduk records
+                Rumah::where('kepala_rumah_tangga', $record->id)->update(['kepala_rumah_tangga' => null]);
+
+                // Delete the KartuKeluarga record
+                $record->delete();
+
+                return redirect('/dasawisma/kartu-keluargas');
+            }),
         ];
     }
 }
--- app/Filament/Dasawisma/Resources/RumahResource/Pages/EditRumah.php
+++ app/Filament/Dasawisma/Resources/RumahResource/Pages/EditRumah.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Dasawisma\Resources\RumahResource\Pages;
 
 use App\Filament\Dasawisma\Resources\RumahResource;
+use App\Http\Middleware\Dasawisma;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use App\Models\Penduduk;
+use App\Models\Rumah;
 
 class EditRumah extends EditRecord
 {
@@ -14,7 +17,16 @@ class EditRumah extends EditRecord
     {
         return [
             Actions\ViewAction::make(),
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->action(function (Rumah $record) {
+                // Update related Penduduk records
+                Penduduk::where('id_rumah', $record->id_rumah)->update(['id_rumah' => null]);
+
+                // Delete the Rumah record
+                $record->delete();
+
+                return redirect('/dasawisma/rumahs');
+            }),
         ];
     }
 }
